7f9e78a40ab962820a8edb9c696d2b048d8881fa
nicklilly93 <nicklilly93@gmail.com>
2016-04-19 15:42:57 -0400

    Added files via upload

--- src/MenuTester.java
+++ src/MenuTester.java
@@ -6,18 +6,21 @@ public class MenuTester
     public static void main(String args[])
     {
         Burger burger = new Burger();
-        BurgerDecorator condiments = new Cheese(new Bacon(burger));
+        //BurgerDecorator condiments = new Cheese(new Bacon());
 
-        //Cheese cheese = new Cheese(burger);
-        //Lettuce lettuce = new Lettuce(burger);
-        //Tomatoes tomatoes = new Tomatoes(burger);
-        //Onions onions = new Onions();
-        //Bacon bacon = new Bacon(burger);
+        Cheese cheese = new Cheese(burger);
+        Lettuce lettuce = new Lettuce(burger);
+        Tomatoes tomatoes = new Tomatoes(burger);
+        Onions onions = new Onions(burger);
+        Bacon bacon = new Bacon(burger);
 
-        System.out.println(condiments.getDescription());
+        //System.out.println(condiments.getDescription());
+
         System.out.println(burger.getFoodName());
         System.out.println(burger.cost());
-        //System.out.println(cheese);
+        System.out.println(cheese.getDescription() + cheese.cost() + "\n" +bacon.getDescription() + bacon.cost() + "\n" + lettuce.getDescription() + lettuce.cost()+ "\n" + tomatoes.getDescription() + tomatoes.cost());
+        double burgerWithEverything = burger.cost() + .75 + .25;
+        System.out.println(burger.getFoodName() + "with everything = " + burgerWithEverything);
         //System.out.println(lettuce);
         //System.out.println(tomatoes);
         //System.out.println(onions);
