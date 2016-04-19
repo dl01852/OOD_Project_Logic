48decf5add3371d768c84735896b420f32a13e9f
nicklilly93 <nicklilly93@gmail.com>
2016-04-19 14:47:38 -0400

    Added files via upload

--- src/MenuTester.java
+++ src/MenuTester.java
@@ -1,33 +1,26 @@
 /**
  * Created by Nicholas on 4/18/2016.
  */
-public class MenuTester extends BurgerDecorator
+public class MenuTester
 {
     public static void main(String args[])
     {
         Burger burger = new Burger();
-        Cheese cheese = new Cheese(burger);
-        Lettuce lettuce = new Lettuce(burger);
-        Tomatoes tomatoes = new Tomatoes(burger);
-        Onions onions = new Onions(burger);
-        Bacon bacon = new Bacon(burger);
+        BurgerDecorator condiments = new Cheese(new Bacon(burger));
 
+        //Cheese cheese = new Cheese(burger);
+        //Lettuce lettuce = new Lettuce(burger);
+        //Tomatoes tomatoes = new Tomatoes(burger);
+        //Onions onions = new Onions();
+        //Bacon bacon = new Bacon(burger);
+
+        System.out.println(condiments.getDescription());
         System.out.println(burger.getFoodName());
         System.out.println(burger.cost());
-        System.out.println(cheese);
-        System.out.println(lettuce);
-        System.out.println(tomatoes);
-        System.out.println(onions);
-        System.out.println(bacon);
-    }
-
-    @Override
-    public String getDescription() {
-        return null;
-    }
-
-    @Override
-    public double cost() {
-        return 1;
+        //System.out.println(cheese);
+        //System.out.println(lettuce);
+        //System.out.println(tomatoes);
+        //System.out.println(onions);
+        //System.out.println(bacon);
     }
 }
